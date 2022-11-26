8bf3152892b7f630474ce190c456a9f682de62bf
NINE_SAI <104677313+NINE-SAI@users.noreply.github.com>
2022-11-26 20:20:50 +0530

    Add files via upload

---
+++ ARRAY_OBJECT/FAT_ARROW_FUNCTION/one.js
@@ -0,0 +1,9 @@
+const a = [
+            {id:101,name:"9SAI",age:22},
+            {id:101,name:"KIRAN",age:32},
+            {id:101,name:"OMMMM",age:72},
+            {id:101,name:"CHANDU",age:30},
+            {id:101,name:"LOKESH",age:12},
+          ]
+const b = a.filter((a)=>a.age < 30)
+console.log(b)

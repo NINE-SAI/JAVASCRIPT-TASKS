3b4480d05c425859018f8a6877389ca5e806fef2
NINE_SAI <104677313+NINE-SAI@users.noreply.github.com>
2022-12-03 22:12:50 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,49 @@
+// let number=10
+// function fibonacciseries(number){
+//     let fibo=[0,1]
+//     for(i=2;i<=number;i++){
+//        fibo[i]=fibo[i-1]+fibo[i-2]
+//     }
+//     return fibo;
+// }
+// console.log(fibonacciseries(number))
+
+// let number = 10
+// function fibonacciseries(number){
+//     let fibo=[0,1]
+//     for(i=2;i<=number;i++){
+//         fibo[i]=fibo[i-1]+fibo[i-2]
+//     }
+//     return fibo
+// }
+// console.log(fibonacciseries(number))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = 10
+function fibonoccisseries(number{
+    let fibo=[0,2]
+    if()
+})
+
+
+
+
+
+

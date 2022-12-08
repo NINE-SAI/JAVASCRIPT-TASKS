d50a672fea6f61e2f918e8cc335434e8dc142e9e
NINE_SAI <104677313+NINE-SAI@users.noreply.github.com>
2022-12-08 22:07:18 +0530

    Add files via upload

---
+++ EVEN/one.js
@@ -0,0 +1,8 @@
+let a = [36,44,35,6,7,8,69,10]
+let b = []
+for(i=0;i>a.length;i=i+1){
+    if(a[i]%2==0){
+        b.push(a[i])
+    }
+}
+console.log(b)
---
+++ EVEN/two.js
@@ -0,0 +1,7 @@
+let a = 223
+if(a%2==0){
+    console.log("IS A EVEN NUMBER")
+}
+else{
+    console.log("IS NOT A EVEN NUMBER")
+}

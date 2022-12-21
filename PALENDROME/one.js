d460f5e4a2c78f75d3b809eaa0c0e5e3c3688c1d
NINE_SAI <104677313+NINE-SAI@users.noreply.github.com>
2022-12-21 11:27:25 +0530

    Add files via upload

---
+++ PALENDROME/one.js
@@ -0,0 +1,9 @@
+const a = "OYO"
+const b = a.toString().split('').reverse().join('')
+const c = a.toString()
+if(b==c){
+    console.log("YES")
+}
+else{
+    console.log("NO")
+}
---
+++ POSITIVE/two.js
@@ -0,0 +1,12 @@
+let number = 5
+if (number >= 0 ){
+    if (number == 0){
+        console.log("ZERO")
+    }
+    else{
+        console.log("POSSITIVE")
+    }
+}
+else{
+    console.log("NEGATIVE")
+}
